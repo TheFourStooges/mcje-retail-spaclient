99a7ce71d090ea7a2f21ce7564e23cdf94ba50c1
pcminh <pcminh@users.noreply.github.com>
2021-11-18 10:27:00 +0700

    WIP 21-11-18

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React, {useState, useEffect} from 'react';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 import Layout from './components/layout/Layout';
 import Home from './components/pages/Home';
+import Cart from './components/pages/Cart/Cart';
 
 function App() {
+  const [cart, setCart] = useState({});
+
+
+
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" elements={<Layout />}>
           <Route index element={<Home/>}/>
-          
+          <Route path="cart" element={<Cart/>}/>
         </Route>
       </Routes>
     </div>
---
+++ src/components/pages/Cart/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+// import CartItem from 
+
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
